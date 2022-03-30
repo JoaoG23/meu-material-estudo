30d4bc4aac4e101729c7f95f3907ae91963cb5ba
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2022-03-30 20:48:35 -0300

    Hooooks Parte 2

--- ReactJS/5-Hooks/my-app/src/App.js
+++ ReactJS/5-Hooks/my-app/src/App.js
@@ -1,12 +1,39 @@
 
+import { useState , useEffect } from 'react';
 import './App.css';
 import Contador from './Contador';
+
 function App() {
-  return (
-    <div>
-      <Contador contador={6}></Contador>
-    </div>
-  );
+  const [show , setShow] = useState(true);
+  
+  let time = 0;
+
+  useEffect(() => {
+
+    let timer = setInterval(() => {
+      time++;
+      console.info(time);
+
+      if ( time > 5 ) {
+        clearInterval(timer);
+        setShow(false);
+      }
+    }, 1000);
+  },[])
+
+  if(show){
+    return (
+      <div>
+        <Contador contador={6}></Contador>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        Sem Contador
+      </div>
+    );
+  }
 }
 
 export default App; //5 - Exportando o App para index.js
--- ReactJS/5-Hooks/my-app/src/Contador.js
+++ ReactJS/5-Hooks/my-app/src/Contador.js
@@ -6,6 +6,26 @@
  * 1- Use effect = Ira ser chamado em 2 ocasioes 
  * 1º Quando o componente for criado. = didMount
  * 2º Toda vez que ele for atualizado = didMountUpdate
+ * 
+ * Estados dos useEffect : Será execultado toda vez que estado 
+ * de uma variavel for recarregada.
+ * 
+ * 1° - Se nao for colocado nada, qualquer estado que for 
+ * atualizado e ira exercultar essa função. ((useEffect))
+ * 
+ * 2º - Se colocar para o estado que voce deseja ele irá execultar
+ * somente para aquele estado. [contador]
+ * 
+ * 3º - Se ele colocar isso vazio esse estado irá exercultar
+ * uma única vez.[]
+ * 
+ * 4º - Para representar o componentWillUnmount, basta
+ * realizar o evento quando o elemento sai da tela.
+ *  Ex: abaixo = Use isso no useEffects 
+ *         return() => {
+            console.info("Não tem mais contador......")
+        }
+
 */
 import React , { useEffect, useState } from "react";
 
@@ -14,15 +34,25 @@ function Contador(props) {
     const [contador , setContador] = useState(props.contador);
     const [meuEstado , setMeuEstado] = useState('qualquer coisa');
 
+    useEffect(() => {
+        setContador(parseInt(localStorage.getItem("contador")));
+
+        return() => {
+            console.info("Não tem mais contador......")
+        }
+
+     },[]) // Para setar o metodo na memoria.
+
     useEffect(() => { 
         document.title = contador // Titulo
-        localStorage.setItem('contador', contador); // LocalStorega
-    })
+        localStorage.setItem("contador", contador); //ERRO - Preciso setar ele da memoria quando a pagina sofre alguma atualização
+    }, [contador]);
 
     function add() {
         setContador(contador + 1);
         console.info(meuEstado);
     }
+
     return (
         <div>
             <h1>Contador: {contador}</h1>
