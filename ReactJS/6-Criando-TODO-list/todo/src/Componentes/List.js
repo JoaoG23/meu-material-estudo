059af2ebaa778bb675d360d315a271b7f71d717c
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2022-04-09 19:46:04 -0300

    Melhorando o Codigo Com Lista de Itens

--- ReactJS/6-Criando-TODO-list/todo/src/Componentes/List.js
+++ ReactJS/6-Criando-TODO-list/todo/src/Componentes/List.js
@@ -1,32 +1,12 @@
 import React from "react";
-import Card from './Card';
 import './List.css';
+import ListItems from './ListItems';
 
-function DoneImg(props) {
-    if ( props.done ) {
-        return (<img alt="done" src="./Assets/feito.svg"></img>)
-    } else {
-        return (<img alt="undone" src="./Assets/naofeito.svg"></img>)
-        
-    }
-}
+// Para que uam props seja exportada coloque pros no componente
+
 
 function List(props) {
-    return(
-                <ul >
-                    {props.items.map(item => <li key={ item.id } >
-                         <Card className={item.done ? "done item" : "item"}>
-                         { item.text }
-                            <div>
-                                <button onClick={ () => { props.onItemChecked(item) }}><DoneImg done={ item.done }></DoneImg> </button>
-                                <button onClick={ () => { props.onItemDeleted(item) }}><img alt="delete" src="./Assets/remove.svg"></img></button>
-                            </div>
-                         </Card>
-                        </li>
-                        )}
-                    
-                </ul>
-    )
+    return(<ul>{ props.items.map(item => <ListItems  key={ item.id } item={ item }  onItemChecked={ props.onItemChecked } onItemDeleted={ props.onItemDeleted } ></ListItems>) }</ul>)
 }
 
 export default List;
---
+++ ReactJS/6-Criando-TODO-list/todo/src/Componentes/ListItems.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from './Card';
+import './List.css';
+
+function DoneImg(props) {
+    if ( props.done ) {
+        return (<img alt="done" src="./Assets/feito.svg"></img>)
+    } else {
+        return (<img alt="undone" src="./Assets/naofeito.svg"></img>)
+    }
+}
+
+
+function ListItems(props) {
+    
+    return(<li>
+                         <Card className={props.item.done ? "done item" : "item"}>
+                            { props.item.text }
+                                <div>
+                                    <button onClick={ () => { props.onItemChecked(props.item) }}><DoneImg done={ props.item.done }></DoneImg> </button>
+                                    <button onClick={ () => { props.onItemDeleted(props.item) }}><img alt="delete" src="./Assets/remove.svg"></img></button>
+                                </div>
+                            </Card>
+                        </li>)
+}
+
+export default ListItems;
