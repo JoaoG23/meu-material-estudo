e78f730a5d1f75d8c349916c90d99a5ed422e0ff
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2022-08-25 20:07:55 -0300

    Comando Sql para visualizuacao de versao Mysql

--- BACKEND/DB/SQL/mySql/aula_mysql_03062019.sql
+++ BACKEND/DB/SQL/mySql/aula_mysql_03062019.sql
@@ -6,3 +6,8 @@ CREATE USER 'novo_usuário'@'localhost' IDENTIFIED BY '1234';
 SHOW GRANTS FOR 'novo_usuário'@'localhost';
 GRANT ALL PRIVILEGES ON supermarket TO 'novo_usuário'@'localhost';
 
+
+Version of System Mysql 
+cmd:
+
+show variables like '%version%';
